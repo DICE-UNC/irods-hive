ade859edabd31cc108a51bc982cb0b15bd3f3a66
Le-Zhang <lezha@live.unc.edu>
2013-09-27 14:28:47 -0400

    fix hiveVocabularyImportException.java

--- hive-code/hive-core/src/edu/unc/ils/mrc/hive/exception/HiveVocabularyImportException.java
+++ hive-code/hive-core/src/edu/unc/ils/mrc/hive/exception/HiveVocabularyImportException.java
@@ -38,15 +38,6 @@ public class HiveVocabularyImportException extends HiveException {
 		super(arg0, arg1);
 	}
 
-	/**
-	 * @param arg0
-	 * @param arg1
-	 * @param arg2
-	 * @param arg3
-	 */
-	public HiveVocabularyImportException(String arg0, Throwable arg1,
-			boolean arg2, boolean arg3) {
-		super(arg0, arg1, arg2, arg3);
-	}
+
 
 }
