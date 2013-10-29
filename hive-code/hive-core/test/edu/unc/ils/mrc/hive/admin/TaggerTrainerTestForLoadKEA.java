b6d79b96ffc1730ba867d4f69e298ee8e9741ff4
mconway <michael_conway@unc.edu>
2013-10-29 09:16:54 -0400

    [#1172] consolidate master project for jargon-hive

--- hive-code/hive-core/test/edu/unc/ils/mrc/hive/admin/TaggerTrainerTestForLoadKEA.java
+++ hive-code/hive-core/test/edu/unc/ils/mrc/hive/admin/TaggerTrainerTestForLoadKEA.java
@@ -30,6 +30,21 @@ public class TaggerTrainerTestForLoadKEA {
 	@BeforeClass
 	public static void setUpBeforeClass() throws Exception {
 		testingProperties = testingPropertiesHelper.getTestProperties();
+		String hivePath = testingProperties
+				.getProperty(HiveTestingPropertiesHelper.TEST_HIVE_PARENT_DIR);
+		SKOSScheme schema = new SKOSSchemeImpl(hivePath, "uat", true);
+
+		schema.importConcepts(schema.getRdfPath(), true, true, true, true, true);
+
+		VocabularyH2 keaH2 = new VocabularyH2(schema, "en");
+		keaH2.initialize();
+
+		// fill in some values from the test properties
+
+		TaggerTrainer trainer = new TaggerTrainer(schema);
+
+		trainer.trainKEAAutomaticIndexingModule();
+		schema.close();
 	}
 
 	/**
@@ -59,7 +74,7 @@ public class TaggerTrainerTestForLoadKEA {
 	 * .
 	 */
 	@Test
-	public void testTrainKEAAutomaticIndexingModule() throws Exception {
+	public void testTagWithKEA() throws Exception {
 
 		String hivePath = testingProperties
 				.getProperty(HiveTestingPropertiesHelper.TEST_HIVE_PARENT_DIR);
