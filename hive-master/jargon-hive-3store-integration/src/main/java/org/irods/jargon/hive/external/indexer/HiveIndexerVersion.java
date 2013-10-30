55358554d6a084d70544d9809d1fb384e535831b
mconway <michael.c.conway@gmail.com>
2013-10-30 13:48:39 -0400

    [#1172] fix pom

--- hive-master/jargon-hive-3store-integration/src/main/java/org/irods/jargon/hive/external/indexer/HiveIndexerVersion.java
+++ hive-master/jargon-hive-3store-integration/src/main/java/org/irods/jargon/hive/external/indexer/HiveIndexerVersion.java
@@ -1,5 +1,5 @@
 package org.irods.jargon.hive.external.indexer;
 public final class HiveIndexerVersion {
  public static String VERSION="1.0-SNAPSHOT";
- public static String BUILD_TIME="20131029-0913";
+ public static String BUILD_TIME="20131030-1347";
 }
