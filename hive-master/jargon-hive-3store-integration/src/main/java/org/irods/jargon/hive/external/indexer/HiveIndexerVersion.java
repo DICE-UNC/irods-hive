8ea6a752b038e800db3b663fbcdb5a2da8eb8406
mconway <michael_conway@unc.edu>
2014-02-27 10:24:42 -0500

    merge latest for le

--- hive-master/jargon-hive-3store-integration/src/main/java/org/irods/jargon/hive/external/indexer/HiveIndexerVersion.java
+++ hive-master/jargon-hive-3store-integration/src/main/java/org/irods/jargon/hive/external/indexer/HiveIndexerVersion.java
@@ -1,21 +1,5 @@
 package org.irods.jargon.hive.external.indexer;
 public final class HiveIndexerVersion {
  public static String VERSION="1.0-SNAPSHOT";
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
- public static String BUILD_TIME="20131022-2330";
-=======
- public static String BUILD_TIME="20131029-0913";
->>>>>>> f0752d04595ae11123516dec1d93a4c98f3f9a09
-=======
- public static String BUILD_TIME="20131030-1347";
->>>>>>> 55358554d6a084d70544d9809d1fb384e535831b
-=======
- public static String BUILD_TIME="20131030-1532";
->>>>>>> 5c3b9df147b9ca5ca296233256ea53a33e9772a6
-=======
- public static String BUILD_TIME="20131111-1523";
->>>>>>> 162d491bb95568d641bf8d363d3bf3b7feaaa1ab
+ public static String BUILD_TIME="20140227-1023";
 }
