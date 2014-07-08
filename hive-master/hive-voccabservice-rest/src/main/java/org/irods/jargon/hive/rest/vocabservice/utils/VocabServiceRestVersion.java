a50d3f1b57c7bec52d996e7f858f149889804fb3
mconway <michael.c.conway@gmail.com>
2014-07-08 15:10:55 -0400

    testing with mao

--- hive-master/hive-voccabservice-rest/src/main/java/org/irods/jargon/hive/rest/vocabservice/utils/VocabServiceRestVersion.java
+++ hive-master/hive-voccabservice-rest/src/main/java/org/irods/jargon/hive/rest/vocabservice/utils/VocabServiceRestVersion.java
@@ -1,5 +1,5 @@
 package org.irods.jargon.hive.rest.vocabservice.utils;
 public final class VocabServiceRestVersion {
  public static String VERSION="1.0-SNAPSHOT";
- public static String BUILD_TIME="20140703-1610";
+ public static String BUILD_TIME="20140708-1450";
 }
